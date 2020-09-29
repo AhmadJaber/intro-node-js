15c2dc306832693dc971ccb6166dcb692e746730
AhmadJaber <jaber131706@gmail.com>
2020-09-29 23:22:58 +0600

    node debugging-testing

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,18 @@
 // write some tests
-describe('users', () => {
-  
-})
+const { fixId, findUser } = require("./users.js");
+
+describe("users", () => {
+	describe("fixId", () => {
+		test("convert param id to db id", () => {
+			expect(fixId("200")).toBe(200);
+		});
+	});
+
+	describe("findUser", () => {
+		test("get user id from id if user is there", async () => {
+			const user = await findUser(1);
+
+			expect(user.id).toBe(1);
+		});
+	});
+});
